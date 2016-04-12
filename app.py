25fe2dee7cc76f3466bdf01cb4d82a893fbbf9c1
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2016-04-12 15:52:06 +0200

    Commit files for Heroku

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, BooleanField, TextAreaField
+from wtforms import validators
+from flask import request, redirect, url_for
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///todo.db')
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    created_at = db.Column(db.DateTime)
+    is_done = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Task {0}, {1!r}>'.format(self.id, self.content)
+
+class TaskForm(Form):
+    content = TextAreaField('Task', [validators.DataRequired()])
+    is_done = BooleanField('Done')
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return  render_template('index.html', tasks=tasks)
+
+@app.route('/createdb')
+def createdb():
+    db.create_all()
+    task1 = Task(content='Write static template with items.')
+    task2 = Task(content='Display items ...')
+    db.session.add(task1)
+    db.session.add(task2)
+    db.session.commit()
+    return 'Done'
+
+@app.route('/task/new', methods=['GET', 'POST'])
+def create_task():
+    form = TaskForm(request.form)
+    if request.method == 'POST' and form.validate():
+        task = Task(content=form.content.data, is_done=form.is_done.data)
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('task_edit.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
